caa29de6f53869b4c58fcdda58a256cf349ab1e0
Mikaya Kim <mikayakim@gmail.com>
2024-03-18 18:41:13 -0500

    org form stuff

---
+++ student-match/src/app/homePage/orgHome.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import "./homePage.css";
+import OrgList from './orgList';
+import OrgForm from './orgForm/orgForm'
+import Activity from "@/app/homePage/types";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const OrgHome: React.FC = () => {
+    const [showOrgForm, setShowOrgForm] = useState(false);
+    const [orgs, setOrgs] = useState<Activity[]>([]);
+
+    const handleOrgSubmit = (org: Activity) => {
+        setOrgs((prevOrgs) => [...prevOrgs, org]);
+        console.log('Activity submitted:', org);
+        setShowOrgForm(false);
+    };
+    const handleButtonClick = () => {
+        setShowOrgForm(true);
+    };
+
+    return (
+        <div className="org_tab">
+            <div style={{ flex: 1, marginRight: '20px' }}>
+                {/* Render the button when ActivityForm is not shown */}
+                {!showOrgForm && (
+                    <button className="org_button" onClick={handleButtonClick}>register a new student org!</button>
+                )}
+            </div>
+            <div style={{ flex: 1 }}>
+                {/* Conditionally render ActivityList or ActivityForm based on state */}
+                {showOrgForm ? (
+                    <Router>
+                        <OrgForm onSubmit={handleOrgSubmit}/>
+                    </Router>
+                ) : (
+                    <OrgList orgs={orgs} />
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default OrgHome;
