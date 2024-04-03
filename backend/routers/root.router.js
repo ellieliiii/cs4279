e5a236782ca88562125797ce0772d9d099e5e8e7
jesszhang782 <jessica.x.zhang@vanderbilt.edu>
2024-04-03 10:15:51 -0500

    added backend code to add roommate forms to mongo

--- backend/routers/root.router.js
+++ backend/routers/root.router.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const userRouter = require('./user.router');
+const express = require("express");
+const userRouter = require("./user.router");
+const roommateRouter = require("./roommate.router");
 
 const RootRouter = express.Router();
 
-RootRouter.get('/', (req, res) => {
-    res.send('Student Match Backend CICD update');
+RootRouter.get("/", (req, res) => {
+  res.send("Student Match Backend CICD update");
 });
 
 // add more routes here
-RootRouter.use('/user', userRouter);
+RootRouter.use("/user", userRouter);
+RootRouter.use("/roommate", roommateRouter);
 
-module.exports = RootRouter
+module.exports = RootRouter;
