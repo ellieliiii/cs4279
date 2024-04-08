dc6ee29232a83542eda348fee13cbba88112f2fa
mpak123 <matthew.pak@vanderbilt.edu>
2024-04-08 00:18:01 -0500

    test1

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const app = require('./index');
 const port = process.env.PORT || 3000;
 
+// Start the server
+
 app.listen(port, () => {
     console.log(`Student Match app listening on port ${port}`);
 })
